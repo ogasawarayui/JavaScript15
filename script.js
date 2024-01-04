96b598849c1dc9f10527dbe224af93269771a8db
ogasawarayui <ogasawara.yui@outlook.com>
2024-01-04 23:46:18 +0900

    js15

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 /* Q1 プロパティーとメソッドの違いをまとめて3つほどメソッドを作ってください。
-値が関数になっているものがメソッド
-呼び出すことができるプロパティ
-オブジェクトのプロパティが関数である場合*/
+値が関数になっているものがメソッド、呼び出すことができるプロパティ*/
+const obj = {
+  methodA: function () {
+    console.log('Hello');
+  },
+  methodB: () => {
+    console.log('Hello');
+  },
+  methodC: () => {
+    console.log('Hello');
+  }
+};
+obj.methodA();
 /* Q2 下記の関数を実行してその結果をコンソールに表示してください。(スコープ内に書く方法はだめです。)*/
-```
 function fruit(name, price) {
   const result = name + "の値段は" + price + "円です。"
   return result
 };
-```
-function tomato(price, func) {
-  const name = "トマト";
-  func(name, price);
-}
-const price = function price(name, price) {
-  console.log(name + " の値段は" + price + "円です。");
-}
-tomato(100, price);
-func(name, price); = price(name, price);
+const re = fruit("トマト", 200);
+console.log(re);
 /* Q3 下記の関数を経由して2つの関数を実行するような処理を作ってください。*/
-```
-function addTax(price, func) {
-  // priceは値段
-  // funcは実行する関数名
-  // 税込み価格を計算して四捨五入して整数にしている(スコープ内でしか参照できません。)
+function addTax(price, func/*, name*/) {
   const taxPrice = Math.round(price * 1.10);
+  func(taxPrice/*, name*/);
 }
-
 // トマトの税込み価格をコンソールに表示させる処理
-
+function tomato(taxPrice) {
+  console.log("トマトの税込み価格は" + taxPrice + "円")
+}
+addTax(200, tomato)
 // 玉ねぎの税込み価格をコンソールに表示させる処理
-```
+function onion(taxPrice) {
+  console.log("玉ねぎの税込み価格は" + taxPrice + "円")
+}
+addTax(250, onion)
 
-
+/*function resultText(taxPrice, name) {
+  console.log(name + "の税込み価格は" + taxPrice + "円")
+}
+addTax(300, resultText, "玉ねぎ")
+addTax(300, resultText, "トマト")*/
 
